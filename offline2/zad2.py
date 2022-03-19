43fc9d0490046011220f82786ecce3a25c51c8af
Kappaprideer <azab@student.agh.edu.pl>
2022-03-19 20:41:47 +0100

    update zadania 2

--- offline2/zad2.py
+++ offline2/zad2.py
@@ -2,8 +2,16 @@ from zad2testy import runtests
 
 
 def depth(L):
-    # tu prosze wpisac wlasna implementacje
-    pass
+    n=len(L)
+    current=0
+    maks=0
+    for i in range(n):
+        current=0
+        for j in range(n):
+            if i!=j and L[j][0]>=L[i][0] and L[j][1]<=L[i][1]:
+                current+=1
+                maks=max(maks, current)
+    return maks
 
 
 runtests( depth ) 
